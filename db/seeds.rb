2fdf6fd67d96f904da6da858a73acf5fff616309
huangqiaoming <carolinehuang.aiesec@gmail.com>
2017-05-01 16:13:44 +0800

    adjust footer position

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 u = User.new
 u.email = "admin@test.com"           # 可以改成自己的 email
 
